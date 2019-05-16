d5b40d84176c56b1cb6e3cfffea0a93603dc3fee
liwenqing <9025wqli@163.com>
2019-05-16 11:26:09 +0800

    工程初始化

---
+++ src/main/java/com/lwq/concurrent/AppMain.java
@@ -0,0 +1,10 @@
+package com.lwq.concurrent;
+
+/**
+ * Created by Administrator on 2019/5/16.
+ */
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
